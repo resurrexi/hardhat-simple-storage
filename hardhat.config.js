9b7e6182a88c81f0f34740556998e139b07e04f1
Liquan Yang <yang.liquan87@gmail.com>
2022-10-01 23:00:58 -0400

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 5, // each network has its own chainId
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,60 @@
+const hre = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+  const SimpleStorageFactory = await hre.ethers.getContractFactory(
+    "SimpleStorage"
+  );
+  console.log("Deploying contract...");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  console.log(`Deployed contract to: ${simpleStorage.address}`);
+
+  // Verify contract if using public network (e.g. Goerli)
+  // Goerli's chainId is 5
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block confirmations...");
+    // Etherscan's may take a moment to discover transaction after deployment
+    // Therefore, wait a few blocks to be mined before verifying
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+
+  // Get current value of contract
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current value is: ${currentValue}`);
+
+  // Update and re-retrieve new value
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated value is: ${updatedValue}`);
+}
+
+async function verify(contractAddress, args) {
+  // only run this if contract is deployed on a public network
+
+  // RESOURCES
+  // https://github.com/NomicFoundation/hardhat/blob/main/packages/hardhat-etherscan/src/constants.ts
+  // https://hardhat.org/hardhat-runner/plugins/nomiclabs-hardhat-etherscan#using-programmatically
+  console.log("Verifying contract...");
+  try {
+    await hre.run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (err) {
+    if (err.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified");
+    } else {
+      console.error(err);
+    }
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
