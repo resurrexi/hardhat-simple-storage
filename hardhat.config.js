67b5c569e6b8c903f5ea110e494aafc3854fde57
Liquan Yang <yang.liquan87@gmail.com>
2022-10-01 23:11:50 -0400

    add custom hardhat task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-etherscan");
+require("./tasks/block-number");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
